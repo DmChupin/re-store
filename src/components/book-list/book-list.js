fcb61c053a2468da199a17769f6a4c580844e955
Дмитрий Чупин <dchupin>
2022-07-10 23:25:00 +0300

    Завершение обучения по Redux.

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,28 +1,70 @@
 import React, {Component} from "react";
 import BookListItem from "../book-list-item";
 import {connect} from "react-redux";
+import {bookAddedToCart, fetchBooks} from '../../actions'
 
 import './book-list.css'
-class BookList extends Component {
-    render() {
-        const {books} = this.props;
-        return (
-            <ul>
-                {
-                    books.map((book) => {
-                        return (
-                            <li key={book.id}>
-                                <BookListItem book={book} />
-                            </li>
-                        );
-                    })
-                }
-            </ul>
-        )
-    }
-}
-const mapStateToProps = ({books}) => {
-    return { books };
+import {withBookStoreService} from "../hoc";
+import {compose} from "../../utils";
+import Spinner from "../spinner";
+import ErrorIndicator from "../error-indicator";
+
+
+const BookList = ({books, onAddedToCart}) => {
+    return (
+        <ul className='book-list'>
+            {
+                books.map((book) => {
+                    return (
+                        <li key={book.id}>
+                            <BookListItem
+                                book={book}
+                                onAddedToCart={() => onAddedToCart(book.id)}
+                            />
+                        </li>
+                    );
+                })
+            }
+        </ul>
+    )
 };
 
-export default connect(mapStateToProps)(BookList);
+class BookListContainer extends Component {
+
+    componentDidMount() {
+        this.props.fetchBooks();
+    }
+
+    render() {
+        const {books, loading, error, onAddedToCart} = this.props;
+
+        if (loading) {
+            return <Spinner/>;
+        }
+
+        if (error) {
+            return <ErrorIndicator />;
+        }
+
+        return <BookList books={books} onAddedToCart={onAddedToCart}/>
+    }
+}
+
+
+const mapStateToProps = ({bookList: {books, loading, error}}) => {
+    return {books, loading, error};
+};
+
+const mapDispatchToProps = (dispatch, {bookStoreService}) => {
+    return {
+        fetchBooks: fetchBooks(dispatch, bookStoreService),
+        onAddedToCart: (id) => {
+            dispatch(bookAddedToCart(id));
+        }
+    }
+}
+
+export default compose(
+    withBookStoreService(),
+    connect(mapStateToProps, mapDispatchToProps)
+)(BookListContainer);
--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,23 +1,28 @@
 export default class BookstoreService {
 
+    data = [
+        {
+            id:1,
+            tittle:'Book 1 Prod',
+            author:'Book 1 author',
+            price: 43,
+            coverImage: 'https://images-na.ssl-images-amazon.com/images/I/617uZq23IPL.jpg',
+        },
+        {
+            id:2,
+            tittle:'Book 2 Prod',
+            author:'Book 3 author',
+            price: 44,
+            coverImage: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL.jpg',
+        }
+    ];
+
     getBooks() {
-        return [
-            {
-                id:1,
-                tittle:'Book 1 Prod',
-                author:'Book 1 author'
-            },
-            {
-                id:2,
-                tittle:'Book 2 Prod',
-                author:'Book 3 author'
-            },
-            {
-                id:3,
-                tittle:'Book 3 Prod',
-                author:'Book 3 author'
-            },
-        ];
+        return new Promise(((resolve, reject) => {
+            setTimeout(() => {
+                resolve(this.data)
+            }, 700);
+        }));
     }
 
 }
